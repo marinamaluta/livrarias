d11c7ad31265b4fac6015935e4991b2ad042a97f
Marina Maluta <marina.queiroz.maluta@gmail.com>
2018-05-05 18:57:33 -0300

    ajuste no modelo e função dos badges

--- conteudos/models.py
+++ conteudos/models.py
@@ -46,18 +46,47 @@ class OBJETIVOS(models.Model):
         return str(self.objetivo)
 
 
+class TEMA(models.Model):
+    tema = models.CharField(max_length=250, verbose_name='Objetivo')
+
+    def __str__(self):
+        return str(self.tema)
+
+
+
 class Material(models.Model):
     titulo = models.CharField(max_length=200, verbose_name='Título')
     desc = models.TextField(max_length=1000, blank=True, verbose_name='Descrição do Título')
-    tipo = models.ManyToManyField(TIPO, blank=True, verbose_name='Tipo')
+    tipo = models.ForeignKey(TIPO, on_delete=models.CASCADE, default='', verbose_name='Tipo')
     ods = models.ManyToManyField(ODS, blank=True, verbose_name='ODS')
     publico_alvo = models.ManyToManyField(PUBLICOALVO, blank=True, verbose_name='Público-alvo')
     faixa_etaria = models.ManyToManyField(FAIXAETARIA, blank=True, verbose_name='Faixa Etaria')
+    tema = models.ForeignKey(TEMA, blank=False, on_delete=models.CASCADE, verbose_name='Tema')
     competencia_bncc = models.ManyToManyField(BNCC, blank=True, verbose_name='Competência BNCC')
     preco = models.CharField(max_length=15, blank=True, verbose_name='Preço')
     objetivos = models.ManyToManyField(OBJETIVOS, blank=True, verbose_name='Objetivos')
     link = models.URLField(blank=True, verbose_name='Link de acesso ao material')
     link_venda =  models.URLField(blank=True, verbose_name='Link de acesso material para venda')
+    imagem_capa = models.URLField(blank=True, verbose_name='URL da imagem de capa')
+
+
+    class Meta:
+        pass
 
     def __str__(self):
         return str(self.titulo)
+
+    def h(self, tag):
+        code = '<span class="badge badge-primary" style="background-color:yellow; color: black;">%s</span>' % (tag)
+        return code
+
+    def tags(self):
+        html = ''
+        for item in self.ods.all():
+            html += self.h(item.codigo)
+        for item in self.publico_alvo.all():
+            html += self.h(item.publico)
+        for item in self.competencia_bncc.all():
+            html += self.h(item.codigo)
+
+        return mark_safe(html)
