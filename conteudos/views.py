c214e8ccbde3281eaeb5660d0f583cb2c1124eca
Marina Maluta <marina.queiroz.maluta@gmail.com>
2018-05-05 12:35:16 -0300

    primeiras views (placeholder)

--- conteudos/views.py
+++ conteudos/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from conteudos.models import Material
+
 
 # Create your views here.
+def index(request):
+    _template = 'conteudos/home.html'
+    materiais = Material.objects.all()
+    _args = {'materiais': materiais}
+    return render(request, template_name=_template, context=_args)
