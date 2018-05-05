99875c68eba67f5c33ea8e771fb3a2e6465a2ade
Marina Maluta <marina.queiroz.maluta@gmail.com>
2018-05-05 18:55:18 -0300

    Primeiras views

--- conteudos/views.py
+++ conteudos/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from conteudos.models import Material
+from conteudos.models import Material, TEMA, TIPO
 
 
 # Create your views here.
 def index(request):
     _template = 'conteudos/home.html'
     materiais = Material.objects.all()
-    _args = {'materiais': materiais}
+    temas = TEMA.objects.all()
+    _args = {'materiais': materiais, 'temas': temas}
+    return render(request, template_name=_template, context=_args)
+
+
+def tema(request, x):
+    _template = 'conteudos/tema.html'
+    tema = TEMA.objects.get(id=x)
+    materiais = Material.objects.filter(tema=tema)
+    tipo = TIPO.objects.all()
+
+    tipo = {}
+    for t in materiais:
+        tipo[t.tipo.tipo] = t.tipo.id
+
+    _args = {'tema': tema, 'tipo': tipo, 'x': x}
+    return render(request, template_name=_template, context=_args)
+
+def tema_tipo(request, x, tipo):
+    _template = 'conteudos/tema-tipo.html'
+    tema = TEMA.objects.get(id=x)
+    tipo = TIPO.objects.get(id=tipo)
+    materiais = Material.objects.filter(tema=tema, tipo=tipo)
+    _args = {'tema': tema, 'materiais': materiais}
     return render(request, template_name=_template, context=_args)
