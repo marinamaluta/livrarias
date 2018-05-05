28c432e6af5ea327884ddb33da843abdab4dad1f
Marina Maluta <marina.queiroz.maluta@gmail.com>
2018-05-05 18:54:21 -0300

    Adiciona URL para navegação

--- conteudos/urls.py
+++ conteudos/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('tema/<int:x>/', views.tema, name='tema-list'),
+    path('tema/<int:x>/<int:tipo>', views.tema_tipo, name='tema-tipo')
 ]
